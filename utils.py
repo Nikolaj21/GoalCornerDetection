65815333a099ec7d893e41a12f53f5460f0dee8a
Nikolaj <nikolajespinoza@yahoo.com>
2023-02-06 09:34:45 +0100

    Final prep

--- utils.py
+++ utils.py
@@ -3,9 +3,9 @@ import os
 ## ALL relevant and often used paths will be put here
 ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
 DATA_DIR = os.path.join(ROOT_DIR,'Data')
-# DATA_DIR_TEST = '../Scratch/s163848/Data_test'
 DATA_DIR_TEST = '/zhome/60/1/118435/Master_Thesis/Scratch/s163848/Data_test'
 
 def export_wandb_api():
-    os.environ['WANDB_API_KEY'] = "fd61955568b424c577dffeebaec0f1a50f1d73be"
+    os.environ['WANDB_API_KEY'] = open('wandb_api.txt','r').read()
+    print(os.environ['WANDB_API_KEY'])
     return print("# Wandb api key successfully exported to environment #")
